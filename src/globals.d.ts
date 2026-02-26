0ad9a35e90dfb01b09ba63de67af17f0c807e26b
rhys-3 <z731014194@gmail.com>
2026-02-26 21:02:34 +0000

    fix

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -7,4 +7,29 @@ declare const toastr: any;
 // 扩展 Element 类型
 interface Element {
   dataset: DOMStringMap;
+  style: CSSStyleDeclaration;
+  offsetWidth: number;
+  offsetHeight: number;
+}
+
+// 扩展 HTMLElement 类型
+interface HTMLElement {
+  width: number;
+  height: number;
+  disabled: boolean;
+}
+
+// HTMLCanvasElement 类型
+interface HTMLCanvasElement {
+  getContext(contextId: '2d'): CanvasRenderingContext2D | null;
+}
+
+// 自定义 Particle 类型
+interface Particle {
+  x: number;
+  y: number;
+  size: number;
+  speedY: number;
+  sway: number;
+  opacity: number;
 }
