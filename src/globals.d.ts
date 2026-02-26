6ae6c38d2ecaebec5f73f8e18b5c0fab6f799745
rhys-3 <z731014194@gmail.com>
2026-02-26 20:57:56 +0000

    fix

---
+++ src/globals.d.ts
@@ -0,0 +1,10 @@
+// 酒馆助手和酒馆提供的全局变量
+declare const jsyaml: any;
+declare const _: any;
+declare const $: any;
+declare const toastr: any;
+
+// 扩展 Element 类型
+interface Element {
+  dataset: DOMStringMap;
+}
